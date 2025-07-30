656d127ecef339051d7201bd7e27efa6b92b58c8
LordofthePickle <powers.gabe@icloud.com>
2025-07-30 15:52:14 +0200

    Implementation for Username and Password validation (in terms of being strong)

--- src/main/java/com/opensourcebim/bcfserver/utils/ValidationUtils.java
+++ src/main/java/com/opensourcebim/bcfserver/utils/ValidationUtils.java
@@ -6,10 +6,22 @@ public class ValidationUtils {
 
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
 
+    private static final Pattern USERNAME_PATTERN = Pattern.compile("^[A-Za-z0-9_]{3,20}$");
+
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$");
+
     private ValidationUtils() {} //security preventing instantiation
 
     public static boolean isValidEmail(String email) {
         return email != null && EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public static boolean isValidUsername(String username) {
+        return username != null && USERNAME_PATTERN.matcher(username).matches();
+    }
+
+    public static boolean isStrongPassword(String password) {
+        return password != null && PASSWORD_PATTERN.matcher(password).matches();
     }
 
 }
