32bdfce3b9ccfce8541b10e926f45276ffe8c3b3
LordofthePickle <powers.gabe@icloud.com>
2025-07-30 15:20:50 +0200

    Creating UserController to call User methods

---
+++ src/main/java/com/opensourcebim/bcfserver/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.opensourcebim.bcfserver.controllers;
+
+import com.opensourcebim.bcfserver.models.User;
+import com.opensourcebim.bcfserver.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<User>> getAllUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(userService.getAllUsers(pageable));
+    }
+}
